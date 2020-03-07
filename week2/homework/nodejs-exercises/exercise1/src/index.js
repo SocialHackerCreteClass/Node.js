37876c19b869dbe6dca713055b6a3b904cef6b90
Konstantinos Antoniadis <hello@pixelattack.gr>
2020-03-07 20:44:38 +0200

    Adds fs module

--- week2/homework/nodejs-exercises/exercise1/src/index.js
+++ week2/homework/nodejs-exercises/exercise1/src/index.js
@@ -1,9 +1,8 @@
+const fs = require('fs')
 const express = require('express')
 
 const app = express()
 const port = process.env.port || 3000
-
-
 
 app.listen(port, () => {
     console.log('Server up and running on port ' + port)
