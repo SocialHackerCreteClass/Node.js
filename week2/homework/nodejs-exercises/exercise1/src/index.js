a1b7f09199e2808dcc296efee02c1fefda35c272
Konstantinos Antoniadis <hello@pixelattack.gr>
2020-03-07 20:25:04 +0200

    Initial commit

---
+++ week2/homework/nodejs-exercises/exercise1/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.port || 3000
+
+
+
+app.listen(port, () => {
+    console.log('Server up and running on port ' + port)
+})
