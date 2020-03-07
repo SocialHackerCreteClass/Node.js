2c962fb96881c51ca78cbb4f13e80d19bacb62cd
Konstantinos Antoniadis <hello@pixelattack.gr>
2020-03-07 21:46:24 +0200

    Sends JSON through Postman and writes file details through fs module

--- week2/homework/nodejs-exercises/exercise1/src/index.js
+++ week2/homework/nodejs-exercises/exercise1/src/index.js
@@ -4,6 +4,17 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
+app.use(express.json())
+
+app.post('/blogs', (req, res) => {
+    const blog = req.body
+    const title = blog.title
+    const content = blog.content
+    res.send(blog)
+    fs.writeFileSync(title, content);
+    res.end('ok')
+})
+
 app.listen(port, () => {
     console.log('Server up and running on port ' + port)
 })
