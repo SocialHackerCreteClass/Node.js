ff0d32200b3bfcceff18f6c343aa51ff6a3a3db0
Konstantinos Antoniadis <hello@pixelattack.gr>
2020-03-11 11:48:36 +0200

    Integrates handlebars, renders templates, adds post handler

--- week2/homework/HackYourTemperature-2/server.js
+++ week2/homework/HackYourTemperature-2/server.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
 app.get('', (req, res) => {
-    res.send('hello from backend to frontend!')
+    res.render('index')
+})
+
+app.post('/weather', (req, res) => {
+    const cityName = req.body.cityName
+    // console.log(cityName)
+    res.send(cityName)
 })
 
 app.listen(3000)
