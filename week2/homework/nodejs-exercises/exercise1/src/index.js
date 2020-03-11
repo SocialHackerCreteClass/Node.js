ec107fc878e763da3c247eb6beac8764d7a44b59
Konstantinos Antoniadis <hello@pixelattack.gr>
2020-03-11 10:11:56 +0200

    Fixes some issues with the express post method

--- week2/homework/nodejs-exercises/exercise1/src/index.js
+++ week2/homework/nodejs-exercises/exercise1/src/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const express = require('express')
-
 const isBlank = require('./utils')
 
 const app = express()
@@ -12,12 +11,39 @@ app.post('/blogs', (req, res) => {
     const blog = req.body
     const title = blog.title
     const content = blog.content
+
     if (isBlank(title) || isBlank(content)) {
-        return res.status(400).send('Error')    
+        return res.status(400).send('Error')
     }
     res.status(201).send(blog)
-    fs.writeFileSync(title, content);
-    res.end('ok')
+    fs.writeFileSync(title, content)
+})
+
+app.patch('/blogs', (req, res) => {
+    const blog = req.body
+    const title = blog.title
+    const content = blog.content
+
+    if (isBlank(content)) {
+        return res.status(400).send('Content cannot be blank')
+    }
+    else if (fs.existsSync(title)) {
+        res.status(200).send(blog)
+        fs.writeFileSync(title, content);
+    }
+    else {
+        res.end('Post does not exist');
+    }
+})
+
+app.delete('/blogs/:title', (req, res) => {
+    const title = req.params.title
+    if (!title) { // DOES NOT WORK
+        return res.status(404).send()
+    }
+    res.send('Deleted: ' + title)
+    fs.unlinkSync(title);
+    res.end('ok');  
 })
 
 app.listen(port, () => {
